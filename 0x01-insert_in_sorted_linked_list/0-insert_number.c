7875568453d9ef65bd74eb3dbaf779dfdf210982
Tukson <santo17338@gmail.com>
2022-08-11 04:35:26 -0300

    insert number ex works locally

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -13,7 +13,7 @@ listint_t *insert_node(listint_t **head, int number)
     listint_t *aux;
     listint_t *new_node;
 
-    if (head == NULL)
+    if (head == NULL && *head == NULL)
     {
         return NULL;
     }
