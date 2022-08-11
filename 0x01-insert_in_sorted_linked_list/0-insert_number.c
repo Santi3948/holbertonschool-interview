c8631e49b60d1299467ccf1131b350ded90a6479
Tukson <santo17338@gmail.com>
2022-08-11 04:32:03 -0300

    insert number ex works locally

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,5 +10,44 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+    listint_t *aux;
+    listint_t *new_node;
 
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    aux = *head;
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = number;
+    if (*head == NULL)
+    {
+        new_node->next = NULL;
+        *head = new_node;
+        return (*head);
+    }
+    while (aux != NULL)
+    {
+        if (number > aux->n && aux->next != NULL && number < aux->next->n)
+        {
+            new_node->next = aux->next;
+            aux->next = new_node;
+            return (new_node);
+        }
+        if (number < aux->n)
+        {
+            new_node->next = aux;
+        }
+        if (aux->next == NULL)
+        {
+            aux->next = new_node;
+            new_node->next = NULL;
+            return (new_node);
+        }
+        aux = aux->next;
+    }
+    return NULL;
 }
