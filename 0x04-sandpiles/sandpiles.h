0f8739e3a649ecbb03b7d55889aa0641d29f2c42
Tukson <santipane@gmail.com>
2022-09-01 20:23:03 -0300

    sandpiles working ok!

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef sandpiles_h_
+#define sandpiles_h_
+#include <stdlib.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int is_not_stable(int grid1[3][3]);
+void toople(int grid1[3][3]);
+void pgrid(int grid[3][3]);
+
+#endif
